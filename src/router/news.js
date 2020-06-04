0a493294f0e7f704c92ce4d1ff073a05549781fc
unknown <hoduykhoa2k@gmail.com>
2020-06-05 00:18:25 +0700

    'done-detail'

--- src/router/news.js
+++ src/router/news.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let router = express.Router();
-let {getDetialNews} = require("./../models/newsModel");
-router.get("/",async(req,res)=>{
+let { getDetialNews,getListNews } = require("./../models/newsModel");
+router.get("/", async (req, res) => {
     res.render("news/index")
 })
-router.get("/*:id",async(req,res)=>{
-    const id = req.params.id
-    const result = await getDetialNews(id)
-    console.log(result)
-    res.render("news/detail")
+router.get("/*.:id", async (req, res) => {
+    const {id} = req.params
+    const [resultDetailNews,resultNews] = await Promise.all([getDetialNews(parseInt(id)),getListNews(1,5)])
+   console.log(resultNews)
+    res.render("news/detail",{resultDetailNews : resultDetailNews,resultNews : resultNews})
 })
-module.exports = router ;
+module.exports = router;
