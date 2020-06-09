4a5b1f61d440fbbf450761b8b5842c2991c2d1c1
unknown <hoduykhoa2k@gmail.com>
2020-06-09 11:50:07 +0700

    'fixproduct'

--- src/router/products.js
+++ src/router/products.js
@@ -6,16 +6,16 @@ router.get("/", async (req, res) => {
     res.render("products/index", { resultData: resultData })
 })
 router.get("/:type", async (req, res) => {
-    const {type} = req.params
-    console.log( req.category)
-    let category  = req.category.filter((item)=>{
-        return item.slug == type ;
+    const { type } = req.params
+
+    let category = req.category.filter((item) => {
+        return item.slug == type;
     })
-    if(category.length ==0){
+    if (category.length == 0) {
         return res.redirect("/");
     }
-    const resultData = await getListProducts(1,10,category[0].id)
-    res.render("products/softWare",{ resultData: resultData ,titleProduct : category[0].name})
+    const resultData = await getListProducts(1, 10, category[0].id)
+    res.render("products/softWare", { resultData: resultData, titleProduct: category[0].name })
 })
 
 module.exports = router;
