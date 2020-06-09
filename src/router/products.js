98f789b8790a3f310cb8e80e6ae03e1ca9181092
unknown <hoduykhoa2k@gmail.com>
2020-06-09 09:20:21 +0700

    'product'

--- src/router/products.js
+++ src/router/products.js
@@ -5,20 +5,17 @@ router.get("/", async (req, res) => {
     const resultData = await getListProducts()
     res.render("products/index", { resultData: resultData })
 })
-router.get("/:facebook", async (req, res) => {
-    // const {facebook} = req.params
-    // const resultData = await getListProducts(facebook)
-    // console.log(resultData)
-    const resultData = await getListProducts()
-    res.render("products/SoftwareFb",{ resultData: resultData })
-})
-router.get("/phan-mem-tiktok", async (req, res) => {
-    const resultData = await getListProducts()
-    res.render("products/SoftwareTikTok",{ resultData: resultData })
-})
-router.get("/phan-mem-zalo", async (req, res) => {
-    const resultData = await getListProducts()
-    res.render("products/SoftwareZalo",{ resultData: resultData })
+router.get("/:type", async (req, res) => {
+    const {type} = req.params
+    console.log( req.category)
+    let category  = req.category.filter((item)=>{
+        return item.slug == type ;
+    })
+    if(category.length ==0){
+        return res.redirect("/");
+    }
+    const resultData = await getListProducts(1,10,category[0].id)
+    res.render("products/softWare",{ resultData: resultData })
 })
 
 module.exports = router;
