311ca5dd363bcc4aaa9a310fc3aa630d377339f1
unknown <hoduykhoa2k@gmail.com>
2020-06-21 00:23:05 +0700

    'listnews'

--- src/router/news.js
+++ src/router/news.js
@@ -1,12 +1,23 @@
 let express = require('express');
 let router = express.Router();
-let { getDetialNews,getListNews } = require("./../models/newsModel");
+let { getDetialNews, getListNews, TotalNumberNews } = require("./../models/newsModel");
 router.get("/", async (req, res) => {
     res.render("news/index")
 })
+router.get("/tin-tuc", async (req, res) => {
+    const page = req.query.page ? req.query.page : 1
+    const [resultListNews, resultTotals] = await Promise.all([getListNews(page, 15), TotalNumberNews()])
+    console.log(resultListNews)
+    if (resultTotals) {
+        pages = resultTotals.count / 12
+    }
+    res.render("news/ListNews", { resultListNews: resultListNews, current: page, pages: pages })
+})
 router.get("/*.:id", async (req, res) => {
-    const {id} = req.params
-    const [resultDetailNews,resultNews] = await Promise.all([getDetialNews(parseInt(id)),getListNews(1,5)])
-    res.render("news/detail",{resultDetailNews : resultDetailNews,resultNews : resultNews})
+    const { id } = req.params
+
+    const [resultDetailNews, resultNews] = await Promise.all([getDetialNews(parseInt(id)), getListNews(1, 5)])
+    console.log(resultDetailNews)
+    res.render("news/detail", { resultDetailNews: resultDetailNews, resultNews: resultNews })
 })
 module.exports = router;
