3efb15c380533a127db352ba63b68f26a24c30f1
unknown <hoduykhoa2k@gmail.com>
2020-06-23 11:09:28 +0700

    'search'

--- src/models/newsModel.js
+++ src/models/newsModel.js
@@ -1,58 +1,85 @@
-const {newsDB,newsFields} = require("./../databases/news");
+const { newsDB, newsFields } = require("./../databases/news");
 const { Op } = require("sequelize");
-const PAGE =1  ;
-const LIMIT_NUMBER= 12 ;
-const createNews = async (data)=>{
-    return newsDB.create({...data});
+const PAGE = 1;
+const LIMIT_NUMBER = 12;
+const createNews = async (data) => {
+    return newsDB.create({ ...data });
 }
-const getListNews = async (page=PAGE,limit=LIMIT_NUMBER,sortBy)=>{
+const getListNews = async (page = PAGE, limit = LIMIT_NUMBER, sortBy) => {
     console.log(sortBy);
-    if(sortBy){
+    if (sortBy) {
         return newsDB.findAll({
-            limit:limit,
-            offset:(page-1)*limit,
-            order:[[sortBy.name,sortBy.sort]],
-            raw:true
-         })
+            limit: limit,
+            offset: (page - 1) * limit,
+            order: [[sortBy.name, sortBy.sort]],
+            raw: true
+        })
     }
     else {
         return newsDB.findAll({
-            limit:limit,
-            offset:(page-1)*limit,
-            raw:true
-         })
+            limit: limit,
+            offset: (page - 1) * limit,
+            raw: true
+        })
     }
-    
+
 }
-const TotalNumberNews = async()=>{
-    return newsDB.findAndCountAll({raw:true});
+const TotalNumberNews = async () => {
+    return newsDB.findAndCountAll({ raw: true });
 }
-const deleteNews = async (id)=>{
+const deleteNews = async (id) => {
     return newsDB.destroy({
-        where:{
-            [newsFields.id]:id
+        where: {
+            [newsFields.id]: id
         }
     })
 }
-const deleteManyNews = async (data)=>{
+const deleteManyNews = async (data) => {
     return newsDB.destroy({
-        where:{
-            [newsFields.id]:{
-                [Op.in]:[...data]
+        where: {
+            [newsFields.id]: {
+                [Op.in]: [...data]
             }
         }
     })
 }
-const getDetialNews = async(id)=>{
+const getDetialNews = async (id) => {
     return newsDB.findOne({
-        where:{[newsFields.id]:id},
-        raw:true
+        where: { [newsFields.id]: id },
+        raw: true
     })
 }
-const updateNews = async(id,data)=>{
-    return newsDB.update({...data},{
-        where:{
-            id:id
+const updateNews = async (id, data) => {
+    return newsDB.update({ ...data }, {
+        where: {
+            id: id
+        }
+    })
+}
+const searchNews = async (data) => {
+    return await newsDB.findAll({
+        where: {
+            [Op.or]: [
+                {
+                    [newsFields.title]: {
+                        [Op.like]: `%${data}%`
+                    },
+                },
+                {
+                    [newsFields.html]: {
+                        [Op.like]: `%${data}%`
+                    }
+                }
+            ]
+        }
+    })
+}
+const TotalNumberNewsSearch = async (data) => {
+    return await newsDB.findAndCountAll({
+        where: {
+            [newsFields.id]: {
+                [Op.in]: [...data]
+            }
         }
     })
 }
@@ -63,5 +90,7 @@ module.exports = {
     deleteNews,
     getDetialNews,
     updateNews,
-    deleteManyNews
+    deleteManyNews,
+    searchNews,
+    TotalNumberNewsSearch
 }
--- src/router/news.js
+++ src/router/news.js
@@ -1,27 +1,49 @@
 let express = require('express');
 let router = express.Router();
-let { getDetialNews, getListNews, TotalNumberNews } = require("./../models/newsModel");
+let { getDetialNews, getListNews, TotalNumberNews, searchNews, TotalNumberNewsSearch } = require("./../models/newsModel");
 // router.get("/", async (req, res) => {
 //     res.render("news/index")
 // })
 router.get("/", async (req, res) => {
+    const Keysearch = req.query.s
     const NUMBER_IN_PAGE = 12;
     const page = req.query.page ? JSON.parse(req.query.page) : 1
-    const [resultListNews, resultTotals] = await Promise.all([getListNews(page, NUMBER_IN_PAGE), TotalNumberNews()])
-    if (resultTotals) {
-        if (resultTotals.count % NUMBER_IN_PAGE == 0) {
-            pages = resultTotals.count / NUMBER_IN_PAGE;
+    let key = ''
+    if (Keysearch) {
+        const resultData = await searchNews(Keysearch)
+        key = Keysearch
+        if (resultData.length > 0) {
+            let arr = []
+            resultData.map(task => {
+                arr.push(task.id)
+            })
+            const resultTotals = await TotalNumberNewsSearch(arr);
+            if (resultTotals.count % NUMBER_IN_PAGE == 0) {
+                pages = resultTotals.count / NUMBER_IN_PAGE;
+            } else {
+                pages = Math.floor(resultTotals.count / NUMBER_IN_PAGE) + 1;
+            }
+            res.render("news/ListNews", { resultListNews: resultData, current: page, pages: pages,key:key })
         } else {
-            pages = Math.floor(resultTotals.count / NUMBER_IN_PAGE) + 1;
+            res.render("news/ListNews", { resultListNews: resultData, current: 0, pages: 0,key:key })
+        }
+    } else {
+        const [resultListNews, resultTotals] = await Promise.all([getListNews(page, NUMBER_IN_PAGE), TotalNumberNews()])
+        if (resultTotals) {
+            if (resultTotals.count % NUMBER_IN_PAGE == 0) {
+                pages = resultTotals.count / NUMBER_IN_PAGE;
+            } else {
+                pages = Math.floor(resultTotals.count / NUMBER_IN_PAGE) + 1;
+            }
+            res.render("news/ListNews", { resultListNews: resultListNews, current: page, pages: pages,key:key })
         }
     }
-    res.render("news/ListNews", { resultListNews: resultListNews, current: page, pages: pages })
+
 })
 router.get("/*.:id", async (req, res) => {
-    const { id } = req.params
 
+    const { id } = req.params
     const [resultDetailNews, resultNews] = await Promise.all([getDetialNews(parseInt(id)), getListNews(1, 6)])
-    console.log(resultDetailNews)
     res.render("news/detail", { resultDetailNews: resultDetailNews, resultNews: resultNews })
 })
 module.exports = router;
